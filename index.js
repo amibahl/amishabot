703d9469d7cc158d2d900538b213ba6ab6be1ae8
Amisha Bahl <abahl@tintri.com>
2016-07-05 00:20:49 -0700

    Reverting token

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'Glacial_Reaches_Amisha_Bot_Testing') {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
